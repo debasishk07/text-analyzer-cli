5178db4293dc51a5da9356081d1433bfa47881ba
Debasish Mohanta <debasishk7809@gmail.com>
2025-06-23 10:48:04 +0530

    added method to read the file

--- text_analyzer.py
+++ text_analyzer.py
@@ -5,3 +5,17 @@ from collections import Counter # to count the occurance
 def remove_punctuations(word):
     return ''.join(char.lower() for char in word if char.isalnum())
 
+
+def check_file(filepath):
+    if not os.path.exists(filepath):
+        print(f" File not found : {filepath}")
+        return
+    
+    with open(filepath, 'r', encoding= 'utf-8') as f :
+        contents = f.read()
+
+    if not contents.strip():
+        print(f"The file '{filepath}' is empty.")
+        return
+    
+    
