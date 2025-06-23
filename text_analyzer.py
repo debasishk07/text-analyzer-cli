e6921c9fd5030dbe9c2c57e5249cffa925ffc71f
Debasish Mohanta <debasishk7809@gmail.com>
2025-06-23 11:19:31 +0530

    styled the document and fixed the line count issue

--- text_analyzer.py
+++ text_analyzer.py
@@ -15,12 +15,13 @@ def check_file(filepath):
         contents = f.read()
 
     if not contents.strip():
-        print(f"The file '{filepath}' is empty.")
+        print(f" The file '{filepath}' is empty.")
         return
     
+    # counting the lines
     lines = contents.splitlines()
-    line_count = len(lines)
-    print(f" Total lines : {line_count}")
+    line_count = contents.count(".") 
+    print(f"\n Total lines : {line_count}")
 
 
     words = [remove_punctuations(word) for line in lines for word in line.split()]
@@ -29,19 +30,20 @@ def check_file(filepath):
     print(f" Total Words: {count_words}")
 
     char_count_with_spaces = len(contents)
-    print(f"Total characters with spaces : {char_count_with_spaces}")
+    print(f" Total characters with spaces : {char_count_with_spaces}")
 
     char_without_spaces = len(contents.replace(" ","").replace("\n",""))
-    print(f"Character without spaces : {char_without_spaces}")
+    print(f" Character without spaces : {char_without_spaces}")
 
     print(f" Top 3 most Frequent words are : ")
     word_freq = Counter(words).most_common(3)
     for word, count in word_freq:
-        print(f"  -{word}: {count}")
+        print(f"> {word} : {count} Times")
     
 # Bonus findings
+    print(f"\n Bonus Findings: ")
     print(f" The average line length : {char_count_with_spaces/line_count:.2f}")
-    print(f"\n average word length is : {sum(len(w) for w in words)/count_words:.2f}")
+    print(f" Average word length is : {sum(len(w) for w in words)/count_words:.2f}")
 
 
 if __name__ == "__main__":
@@ -49,3 +51,5 @@ if __name__ == "__main__":
         print("usage: python text_analyzer.py <file.txt>")
     else:
         check_file(sys.argv[1])
+
+print("\n")
